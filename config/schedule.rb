55247d9f24571849fd878c5a20015367924825b6
Piotr Kędziak <piotr@kedziak.com>
2023-02-22 19:11:16 +0100

    Add whenever gem

---
+++ config/schedule.rb
@@ -0,0 +1,10 @@
+# Use this file to easily define all of your cron jobs.
+#
+# It's helpful, but not entirely necessary to understand cron before proceeding.
+# http://en.wikipedia.org/wiki/Cron
+
+# Learn more: http://github.com/javan/whenever
+
+every :monday, at: '12pm' do
+  # runner "Task.do_something_great"
+end
